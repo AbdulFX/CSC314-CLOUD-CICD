d4d078c45efd293d90307f9f838d2944292de029
Abdullahi Mohammed <elemohammed202@gmail.com>
2024-05-27 12:24:31 +0100

    Changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send(' this is CSC 314 cloud computing DEVOPS ; CI-CD PIPELINE !'));
+app.get('/', (req, res) => res.send(' this is CSC 314 cloud computing DEVOPS ; CI-CD PIPELINE CREATED WITH AWS. YHIS PAGE INDICATES SUCCESS. OBI ISRAEL CHIBUIKE AND ABDULLAHI MOHAMMED, VUG/CS/21/5208 AND VUG/CSC/21/5157 '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
